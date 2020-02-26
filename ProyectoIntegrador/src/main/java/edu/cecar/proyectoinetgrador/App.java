083efcb1bbd57690a40d52000e767a2294ec1b3d
1103123503 <1103123503@SJLBF02050520.admcecar.local>
2020-02-26 16:54:08 -0500

    se definio la estructura del proyecto.

---
+++ ProyectoIntegrador/src/main/java/edu/cecar/proyectoinetgrador/App.java
@@ -0,0 +1,13 @@
+package edu.cecar.proyectoinetgrador;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
